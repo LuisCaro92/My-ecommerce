9bd9076e41a60b847c2c4ef1092d358a4b414d35
luis caro <luis.carosalgado1@gmail.com>
2023-09-18 00:48:23 -0300

    agregando modificaciones a nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import { BeakerIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { BeakerIcon, Bars3Icon, XMarkIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 function Nav() {
@@ -24,9 +24,12 @@ function Nav() {
 
           <ul className={`sm:flex pl-9 sm:pl-0 sm:items-center sm:pb-0 pb-12 absolute sm:static sm:z-auto z-[-1] left-0 w-full transition-all bg-white duration-500 ease-in 
           ${isOpen ? "top-12" : "top-[-490px]"}`}>
+            <li> <input className="w-80 h-10 border rounded-md mr-60" type="text" placeholder="Buscar"/></li>
             {Links.map((link) => (
               <li className="my-7 sm:my-0 sm:ml-8">{link.name}</li>
+             
             ))}
+             
             <button className="btn bg-indigo-500 text-white py-1 px-8  rounded-md sm:ml-8 sm:static">
               SingIn
             </button>
