a7a19212cba14406bc7e09ba4a986204203f7b65
luis caro <luis.carosalgado1@gmail.com>
2023-11-24 14:46:39 -0300

    estancado

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,19 +7,6 @@ function Home() {
         <div className="grid grid-cols-1 m-10 gap-3 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3  items-center justify-center">
        
           <Card className="col-span-2"/>
-                 
-          <div className=" bg-green-300">
-                columna2
-          </div>
-          <div className=" bg-yellow-300">
-                columna3
-          </div>
-          <div className=" bg-red-500">
-                columna3
-          </div>
-          <div className=" bg-gray-500">
-                columna3
-          </div>
         </div>
       </div>
     </div>
