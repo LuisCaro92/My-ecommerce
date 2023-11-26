c7ffd3a374f8c0bd6d1c0fdc5cbe251ea0d126ad
luis caro <luis.carosalgado1@gmail.com>
2023-11-25 21:57:56 -0300

    creando base de datos

---
+++ src/components/card/Data.js
@@ -0,0 +1,10 @@
+
+export const data =[
+    {
+        id:1,
+        name:"Nombre",
+        stock:10,
+        precio:40000
+        
+    }
+]
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,6 @@ function Home() {
     <div className="">s
       <div className="container">
         <div className="grid grid-cols-1 m-10 gap-3 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3  items-center justify-center">
-       
           <Card className="col-span-2"/>
         </div>
       </div>
