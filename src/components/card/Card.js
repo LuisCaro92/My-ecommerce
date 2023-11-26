e91a8347922523865f66869701843cb1488e2304
luis caro <luis.carosalgado1@gmail.com>
2023-11-25 23:41:33 -0300

    ultimos detalles de card terminados

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,42 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
+import { data } from './Data';
 
-const ProductCard = ({ data }) => {
+const  ProductCard = ()=> {
   return (
     <Fragment>
       {data.map((product) => (
-        <div
-          className="text-gray w-100 shadow-white shadow-lg rounded-md overflow-hidden mt-10"
-          key={product.id}
-        >
-          <img
-            className="w-full h-full object-cover"
-            src={product.imagen}
-            alt={product.name}
-          />
+        <div className="text-gray  shadow-white shadow-lg rounded-md overflow-hidden mt-10" key={product.id}>
+          <img className="w-full h-60 object-fill" src={product.imagen} alt={product.name} />
           <div className="bg-white p-5 flex flex-col gap-2 ">
             <div className="flex items-center justify-between gap-2 ">
               <h2 className="font-semibold text-2xl overflow-ellipsis overflow-hidden whitespace-nowrap">
                 {product.name}
               </h2>
               <div>
-                <span className="px-3 mr-4  py-1 rounded-full text-xs text-white bg-[#3C3D50]">
-                  Stock
-                </span>
-                <span className="px-3  py-1 rounded-full text-xs text-white bg-[#3C3D50]">
-                  {product.stock}
-                </span>
+                <span className="px-3 mr-4  py-1 rounded-full text-xs text-white bg-[#3C3D50]">Stock</span>
+                <span className="px-3  py-1 rounded-full text-xs text-white bg-[#3C3D50]">{product.stock}</span>
               </div>
             </div>
 
             <div className="flex justify-between">
               <span className="text-xl font-bold"> ${product.precio}</span>
               <div className="flex items-center gap-2 mt-1">
-                <span className="text-sm line-through opacity-50">
-                  $65.0000
-                </span>
-                <span className="bg-[#3C3D50] px-1.5 py-0.5 rounded-md text-xs text-white">
-                  Save 20%
-                </span>
+                <span className="text-sm line-through opacity-50">$65.0000</span>
+                <span className="bg-[#3C3D50] px-1.5 py-0.5 rounded-md text-xs text-white">Save 20%</span>
               </div>
             </div>
 
@@ -54,6 +40,6 @@ const ProductCard = ({ data }) => {
       ))}
     </Fragment>
   );
-};
+}
 
 export default ProductCard;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,59 +1,65 @@
-import { BeakerIcon, Bars3Icon, XMarkIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
-import { Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import { BeakerIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
-
-const  Nav = ()=> {
+const Nav = () => {
   const links = ["Home", "Profile", "Setting"];
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Fragment>
-    <div className="shadow-md w-full fixed top-0 left-0 ">
-      <div className="bg-[#1F1C3F] sm:px-10 py-4 px-7 sm:flex justify-between items-center">
-        {/* Logo */}
-        <Link to="/Home">
-        <div className="flex text-2xl cursor-pointer items-center gap-2">
-          <BeakerIcon className="w-7 h-7 text-[#B5E6DB]" />
-          <span className="font-bold text-white">Ecommerce</span>
-        </div>
-        </Link>
-        {/* Menu */}
-        <div className="flex pl-9 md:pl-0 font-semibold text-white ">
-          <div
-            onClick={() => setIsOpen(!isOpen)}
-            className="w-7 h-7 absolute right-8 top-6 cursor-pointer sm:hidden"
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="bg-[#1F1C3F] sm:px-10 py-4 px-7 sm:flex justify-between items-center">
+          {/* Logo */}
+          <Link
+            to="/Home"
+            className="flex text-2xl cursor-pointer items-center gap-2"
           >
-            {isOpen ? <XMarkIcon /> : <Bars3Icon />}
-          </div>
+            <BeakerIcon className="w-7 h-7 text-[#B5E6DB]" />
+            <span className="font-bold text-white">Ecommerce</span>
+          </Link>
 
-          <ul
-            className={`sm:flex pl-9 sm:pl-0 sm:items-center sm:pb-0 pb-12 absolute sm:static sm:z-auto z-[-1] left-0 w-full transition-all bg-[#1F1C3F] duration-500 ease-in 
-            ${isOpen ? "top-12" : "top-[-490px]"}`}
-          >
-            <li>
-              <input
-                className="w-80 h-10 border rounded-md mr-20 text-center "
-                type="text"
-                placeholder="Buscar"
-              />
-            </li>
-            {links.map((link, index) => (
-              <li key={index} className={`my-7 sm:my-0 ${index !== 0 ? "sm:ml-8" : ""}`}>
-                {link}
+          {/* Menu */}
+          <div className="flex pl-9 md:pl-0 font-semibold text-white">
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className="w-7 h-7 absolute right-8 top-6 cursor-pointer sm:hidden"
+            >
+              {isOpen ? <XMarkIcon /> : <Bars3Icon />}
+            </div>
+
+            <ul
+              className={`sm:flex pl-9 sm:pl-0 sm:items-center sm:pb-0 pb-12 absolute sm:static sm:z-auto z-[-1] left-0 w-full transition-all bg-[#1F1C3F] duration-500 ease-in 
+                ${isOpen ? "top-12" : "top-[-490px]"}`}
+            >
+              <li>
+                <input
+                  className="w-80 h-10 border rounded-md mr-20 text-center"
+                  type="text"
+                  placeholder="Buscar"
+                />
               </li>
-            ))}
-           <Link to="/Login">
-            <button className=" bg-[#B5E6DB] text-black py-1 px-8 rounded-md sm:ml-8 sm:static">
-              Sign In
-            </button>
-            </Link>
-            
-          </ul>
+
+              {links.map((link, index) => (
+                <li
+                  key={index}
+                  className={`my-7 sm:my-0 ${index !== 0 ? "sm:ml-8" : ""}`}
+                >
+                  {link}
+                </li>
+              ))}
+
+              <Link to="/Login">
+                <button className="bg-[#B5E6DB] text-black py-1 px-8 rounded-md sm:ml-8 sm:static">
+                  Sign In
+                </button>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </Fragment>
   );
-}
+};
+
 export default Nav;
